5d65d38481fcf5e9678831eb6b13c93862aa4701
davidGarcia <djgj57@hotmail.com>
2020-12-14 15:46:26 -0500

    con nuevo home

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,29 +1,30 @@
 <template>
-<div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon "></span>
-  </button>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon "></span>
+      </button>
 
-  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0">
+          <button class="btn btn-danger" type="button" @click="logOut">Salir</button>
+        </form>
+      </div>
+    </nav>
 
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="button" @click="logOut">Salir</button>
-    </form>
+    <div class = "container col-sm-4">
+      <div class="card bg-dark w-600px">
+        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title text-white">username: {{ user.name }}</h5>
+            <p class="card-text text-white">email: {{ user.email }}</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Grupo 64 / Ciclo 3 / Mision TIC 2022</small>
+        </div>
+      </div>
+    </div>
   </div>
-</nav>
-
-<div>
- <div class="card col-sm-6 w-50">
-  <h5 class="card-header">{{user.name}}</h5>
-  <div class="card-body">
-    <h5 class="card-title">Correo:</h5>
-    <p class="card-text">{{user.email}}</p>
-  </div>
-</div>
-</div>
-
-</div>
 </template>
 
 <script>
