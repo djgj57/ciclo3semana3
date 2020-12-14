741848a2cf1231b5a4ac3f7b846663084f7fb184
davidGarcia <djgj57@hotmail.com>
2020-12-14 10:07:29 -0500

    Funcionando con Login&Home bonitos

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,12 +1,11 @@
 <template>
 <div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon "></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
 
     <form class="form-inline my-2 my-lg-0">
       <button class="btn btn-outline-success my-2 my-sm-0" type="button" @click="logOut">Salir</button>
@@ -14,15 +13,15 @@
   </div>
 </nav>
 
-<div class="container">
-<p>
-    {{user.nombre}}
-</p>
-<p>
-    {{user.email}}
-</p>
+<div>
+ <div class="card col-sm-6 w-50">
+  <h5 class="card-header">{{user.name}}</h5>
+  <div class="card-body">
+    <h5 class="card-title">Correo:</h5>
+    <p class="card-text">{{user.email}}</p>
+  </div>
 </div>
-
+</div>
 
 </div>
 </template>
@@ -57,7 +56,3 @@ export default {
 }
 </script>
 
-
-<style scoped>
-
-</style>
